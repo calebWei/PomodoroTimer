903facde8f8c6e017de52c2d0a1c9a16a0803cb4
calebWei <wei8vzgk@gmail.com>
2022-11-07 18:04:24 +1300

    initial  commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.pomodorotimer {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.pomodorotimer to javafx.fxml;
+    exports com.example.pomodorotimer;
+}

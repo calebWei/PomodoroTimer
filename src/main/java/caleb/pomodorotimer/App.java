a08a0c450271f99bc39dfd8dbd4d176a9372b97b
calebWei <wei8vzgk@gmail.com>
2022-11-09 14:43:27 +1300

    add - work.fxml visual done.

--- src/main/java/caleb/pomodorotimer/App.java
+++ src/main/java/caleb/pomodorotimer/App.java
@@ -5,6 +5,7 @@ import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
@@ -17,9 +18,14 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        // Get font
+        Font.loadFont(getClass().getResourceAsStream("/fonts/Dosis-Regular.ttf"), 0);
+        // Load first scene
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/fxml/work.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 970, 650);
         stage.setTitle("Pomodoro Timer");
+        // Load css stylesheet
+        scene.getStylesheets().add("/css/Default.css");
         stage.setScene(scene);
         stage.show();
 

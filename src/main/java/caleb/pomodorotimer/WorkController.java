0ef3c6bb88f44992a70c8b908bda6108db83cbaa
calebWei <wei8vzgk@gmail.com>
2022-11-09 16:29:12 +1300

    implement - start button completely functional.

--- src/main/java/caleb/pomodorotimer/WorkController.java
+++ src/main/java/caleb/pomodorotimer/WorkController.java
@@ -9,28 +9,55 @@ import javafx.util.Duration;
 
 public class WorkController {
     @FXML
-    private Label timerClock, startLabel;
+    private Label timerClock, startLabel, statLabel, settingLabel;
     @FXML
-    private Rectangle startButton, skipButton, statButton, settingButton;
+    private Rectangle skipButton, statButton, settingButton;
     private Timeline timeline;
     private int time = 1800;
+    enum State {
+        STOP,
+        RUN,
+        PAUSE
+    }
+    private State state = State.STOP;
 
     @FXML
     // Start the timer when the start button is pressed
     private void onStartButton() {
-        // If the timer hasn't started, start the timer
-        // If the timer is running, pause the timer
-        // If the timer is paused, continue the timer
-        // Set timelines for timer and predict
-        System.out.println("start timer");
-        timeline = new Timeline(new KeyFrame(Duration.millis(1000), e -> decrementTime()));
-        timeline.setCycleCount(time);
-        timeline.play();
+        if (state == State.STOP){
+            // If the timer hasn't started, start the timer
+            System.out.println("start timer");
+            timeline = new Timeline(new KeyFrame(Duration.millis(1000), e -> decrementTime()));
+            timeline.setCycleCount(time);
+            timeline.play();
+            state = State.RUN;
+            // Enable/ Disable buttons
+            skipButton.setDisable(false);
+            statButton.setDisable(true);
+            statButton.setVisible(false);
+            statLabel.setVisible(false);
+            settingButton.setDisable(true);
+            settingButton.setVisible(false);
+            settingLabel.setVisible(false);
+            startLabel.setText("Pause");
+        } else if (state == State.RUN) {
+            // If the timer is running, pause the timer
+            System.out.println("pause timer");
+            timeline.pause();
+            state = State.PAUSE;
+            startLabel.setText("Continue");
+        } else if (state == State.PAUSE) {
+            // If the timer is paused, continue the timer
+            System.out.println("start timer");
+            timeline.play();
+            state = State.RUN;
+            startLabel.setText("Pause");
+        }
     }
 
     private void decrementTime(){
-        timerClock.setText(secondsToHMS(time));
         time--;
+        timerClock.setText(secondsToHMS(time));
     }
 
     private String secondsToHMS(int timeInSeconds) {
